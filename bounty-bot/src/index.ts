3c73325bc3c36f3495d6f7e4133e50bb6607491c
Piyush <sainipiyush8860@gmail.com>
2024-05-06 19:11:43 +0530

    initial commit

---
+++ bounty-bot/src/index.ts
@@ -0,0 +1,21 @@
+import { Probot } from "probot";
+
+export default (app: Probot) => {
+  app.on("issues.opened", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue!",
+    });
+    await context.octokit.issues.createComment(issueComment);
+  });
+
+  app.on("push", async (context) => {
+    // Code was pushed to the repo, what should we do with it?
+    console.log("control was here")
+    app.log.info(context);
+  });
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+};
