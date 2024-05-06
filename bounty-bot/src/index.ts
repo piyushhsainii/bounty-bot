635108efd0c0d62232a9509ad20351a76bae2770
Piyush <sainipiyush8860@gmail.com>
2024-05-06 20:17:11 +0530

    Initial bot setup done

--- bounty-bot/src/index.ts
+++ bounty-bot/src/index.ts
@@ -4,7 +4,6 @@ export default (app: Probot) => {
 
   app.log.level = "debug"
   app.log.info("hello")
-  console.log("hello")
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
@@ -12,15 +11,24 @@ export default (app: Probot) => {
     await context.octokit.issues.createComment(issueComment);
   });
  
+    app.on("pull_request_review_comment",async(context)=>{
+      const prComment = context.issue({body:"thanks for review this PR"})
+      return await context.octokit.issues.createComment(prComment)
+    })
+    
+    app.on("issue_comment",async(context)=>{
+      if (context.payload.comment.user.type === "Bot") {
+        return;
+      }
+      app.log.debug("control is in here")
+      const prComment = context.issue({body:"thanks for review this PR"})
+      return  await context.octokit.issues.createComment(prComment)
+    })
+
   app.on("push", async (context) => {
     // Code was pushed to the repo, what should we do with it?
     console.log("control was here")
     app.log.info("control was here")
     app.log.info(context);
   });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
