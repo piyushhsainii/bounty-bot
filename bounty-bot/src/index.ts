7a4adf81b8441435954b8736435b8efa7491a0ec
Piyush <sainipiyush8860@gmail.com>
2024-05-06 19:26:45 +0530

    pushing code to test event handlers

--- bounty-bot/src/index.ts
+++ bounty-bot/src/index.ts
@@ -1,16 +1,21 @@
 import { Probot } from "probot";
 
 export default (app: Probot) => {
+
+  app.log.level = "debug"
+  app.log.info("hello")
+  console.log("hello")
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
     await context.octokit.issues.createComment(issueComment);
   });
-
+ 
   app.on("push", async (context) => {
     // Code was pushed to the repo, what should we do with it?
     console.log("control was here")
+    app.log.info("control was here")
     app.log.info(context);
   });
   // For more information on building apps:
